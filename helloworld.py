ad26f79ac981e210133ebdd43c109b260aa6bf6e
Mark Offerman <m.a.j.offerman@gmail.com>
2017-07-13 17:30:13 +0200

    First experiments with variables

--- helloworld.py
+++ helloworld.py
@@ -1,4 +1,29 @@
-import tensorflow as tf 
+#!/usr/bin/env python3
+import os
+import tensorflow as tf
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-my_const = tf.constant([1.0,2.0], name = "my_const")
-print(tf.get_default_graph().as_graph_def())
+# create variable with scalar
+
+a = tf.Variable(2, name = "scalar")
+
+# create variable with vector
+
+b = tf.Variable([2,2], name = "vector")
+
+# create variable as matrix
+
+c = tf.Variable([[2,2], [3,2]], name = "matrix")
+
+# create variable W with a 728 x 10 tensor, filled with zero's
+
+W = tf.Variable(tf.zeros([728,10])) 
+
+#only initiating but not fetching
+#init = tf.global_variables_initializer()
+
+init_ab = tf.variables_initializer([a, b], name = "init_ab")
+
+with tf.Session() as sess:
+	sess.run(init_ab)
+	print(W.eval())
