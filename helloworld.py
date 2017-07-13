e9c5bd853eacd5c10a405c0e1246428a9bc0b4fd
Mark Offerman <m.a.j.offerman@gmail.com>
2017-07-13 13:40:46 +0200

    Added list to constants plus naming

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
-a = tf.constant(3)
-b = tf.constant(2)
-x = tf.add(a, b)
+a = tf.constant([2, 2], name = "a")
+b = tf.constant([3, 6], name = "b")
+x = tf.add(a, b, name = "add")
 
 with tf.Session() as sess:
 	writer = tf.summary.FileWriter('./graphs', sess.graph)
